4ba8d2a8371489e17368c3df2213573f9f159976
Karima Jarmoumi <kjarmoum@c3r2p7.1337.ma>
2024-08-03 15:35:13 +0100

    feat: chat added to single page

--- test.js
+++ test.js
@@ -1,162 +1,3 @@
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: myFont;
-}
-
-@font-face {
-    font-family: myFont;
-    src: url(Season\ Prime.otf);
-}
-
-body{
-    background: #161C40;
-    width: 100vw;
-    height: 100vh;
-    display: grid;
-    grid-template-rows: 8% 90%;
-    /* border: 1px solid red; */
-}
-
-header{
-    padding: 6px 25px;
-    /* border: 1px solid yellow; */
-}
-
-nav{
-    display: flex;
-    align-items: center;
-}
-
-/* --------logo-------------- */
-
-.logo{
-    width: 80px;
-    height: 50px;
-    cursor: pointer;
-    transition: filter 0.3s ease;
-    padding-left: 2px;
-    /* border: 1px solid green; */
-}
-
-.logo:hover{
-    width: 87px;
-    height: 57px;
-    filter: blur(0.5px);
-    transition: 0.5s;
-}
-
-.logo:active{
-    width: 80px;
-    height: 57px;
-}
-
-/* ------------search------------ */
-
-.search{
-    width: 340px;
-    height: 35px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-left: 120px;
-    cursor: pointer;
-    border-radius: 27px;
-    background: #135B8B;
-    padding: 16px;
-    box-shadow: 0 2px 2px rgba(52, 81, 143, 33);
-    transition: box-shadow 0.5s;
-    filter: blur(0.5px);
-}
-
-.search:focus-within{
-    box-shadow: 0 0 2px #5293CB;
-}
-
-.search input{
-    font-size: 16px;
-    font-family: 'Josefin Sans';
-    color: #ffffff;
-    margin-left: 14px;
-    outline: none;
-    border: none;
-    background: transparent;
-    width: 300px;
-}
-
-.search input::placeholder{
-    color: #5293CB;
-    font-size: 14px;
-}
-
-/* --------notif-setting-logout------------ */
-
-.left-side{
-    width: 100%;
-    height: 49px;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    gap: 1em;
-    /* border: 1px solid yellow; */
-}
-
-i{
-    color: #5293CB;
-    cursor: pointer;
-    font-size: 24px;
-    transition: color 0.4s ease, filter 0.4s ease;
-}
-
-.container{
-    height: auto;
-    display: grid;
-    grid-template-columns: 7% 84% 7% ;
-    column-gap: 1em;
-    /* border: 1px solid green; */
-}
-
-.side-bar{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    gap: 2em;
-    /* border: 1px solid rgb(0, 255, 238); */
-}
-
-.friends{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    /* border: 1px solid rgb(234, 1, 255); */
-}
-
-/* -----------Icons------------------ */
-
-i{
-    color: #5293CB;
-    cursor: pointer;
-    font-size: 24px;
-    transition: color 0.4s ease-in-out, filter 0.4s ease-in-out;
-}
-
-i:hover{
-    color: #F45250;
-    font-size: 30px;
-    filter: blur(0.5px);
-    transition: 0.5s;
-} 
-
-.icon.clicked{
-    color: #F45250;
-    font-size: 30px;
-    filter: blur(0.75px);
-}
-
-/* ----------- chat content ------------------ */
-
 .chat-content{
     background-color: rgba(217, 217, 217, 8%);
     border-radius: 56px;
@@ -164,9 +5,11 @@ i:hover{
     grid-template-columns: 25% 74%;
     display: grid;
     gap:1em; 
+    height: 100%;
 }
-    /* ----------- chat list ----------- */
-.chat-list-container{
+
+/* ----------- chat list ----------- */
+.chat-content .chat-list-container{
     background-color: #FFEEBF;
     border-radius: 56px;
     padding: 3px;
@@ -174,17 +17,13 @@ i:hover{
     display: grid;
 }
 
-.chat-list-container .clicked-chat {
-    background-color: rgba(208, 210, 217, 0.514);
-}
-
-.chat-list-container .separator{
+.chat-content .chat-list-container .separator{
     background-color: rgba(208, 210, 217, 0.61);
     border-radius: 56px;
 }
 
     /* ----------- chat list header ----------- */
-.chat-list-header{
+.chat-content .chat-list-container .chat-list-header{
     background-color: transparent;
     font-family: myFont;
     /* border: 1px solid rgb(255, 124, 1);  */
@@ -195,7 +34,7 @@ i:hover{
     justify-content: center;
 } 
 
-.chat-list-header .fa-rocketchat{
+.chat-content .chat-list-container .chat-list-header .fa-rocketchat{
     margin: 2px;
     font-size: xx-large;
     color: rgba(10, 70, 110, 1);
@@ -203,18 +42,16 @@ i:hover{
 
     /* ----------- chat list items ----------- */
 
-.chat-list-items{
+.chat-content .chat-list-container .chat-list-items{
     padding: 5px;
-    /* border: 1px solid rgb(9, 201, 3); */
     border-radius: 56px;
     grid-template-rows: repeat(9, 10%);
     display: grid;
     gap: 0.2em;
 }
 
-.chat-item{
+.chat-content .chat-list-container .chat-list-items .chat-item{
     padding: 5px;
-    /* border: 1px solid rgba(1, 43, 158, 0.068); */
     border-radius: 20px;
     grid-template-columns: 18% 80%;
     display: grid;
@@ -222,13 +59,14 @@ i:hover{
     box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.185);
 
 } 
- .chat-item:hover{
+ 
+.chat-content .chat-list-container .chat-list-items .chat-item:hover{
     background-color: rgba(208, 210, 217, 0.514);
- }
+}
 
     /* ----------- avatar ----------- */
 
-.contact-avatar{
+.chat-content .chat-list-container .chat-list-items .chat-item .contact-avatar{
     /* border: 1px solid rgb(255, 255, 255); */
     border-radius: 50px;
     width: 60px;
@@ -237,19 +75,19 @@ i:hover{
     
 }
 
-.contact-box{
+.chat-content .chat-list-container .chat-list-items .chat-item .contact-box{
     grid-template-rows: 50% 50%;
     display:  grid;
 }
 
 
-.contact-box-header{
+.chat-content .chat-list-items .chat-item .contact-box .contact-box-header{
     grid-template-columns: 60% 40%;
     display:  grid;
 }
 
 /* ----------- name  ----------- */
-.contact-name{
+.chat-content .chat-list-items .chat-item .contact-box .contact-box-header .contact-name{
     padding: 5px;
     font-size: large;
     font-weight: bold;
@@ -257,7 +95,7 @@ i:hover{
     color: rgba(10, 70, 110, 1);
 }
 
-.time-last-message{
+.chat-content .chat-list-items .chat-item .contact-box .contact-box-header .time-last-message{
     padding: 8px;
     /* border: 1px solid rgb(255, 251, 2); */
     font-size:medium;
@@ -270,7 +108,7 @@ i:hover{
 
 /* ----------- last msg  ----------- */
 
-.last-message{
+.chat-content .chat-list-items .chat-item .contact-box .last-message{
     color: rgba(155, 155, 155, 1);
     border-radius: 70px;
     padding-left: 5px;
@@ -279,14 +117,14 @@ i:hover{
 
 /* ----------- time of the last msg  ----------- */
 
-.message-list{
+.chat-content .message-list{
     background-color: #D2D4DA;
     border-radius: 56px;
     grid-template-rows: 10% 0.5% 74% 15.5%;
     display: grid;
 } 
 
-.message-list .header{
+.chat-content .message-list .header{
     background-color: transparent;
     padding: 5px;
     padding-left: 20px;
@@ -296,27 +134,27 @@ i:hover{
     
 } 
 
-.message-list .header .contact-avatar{
+.chat-content .message-list .header .contact-avatar{
     margin-top: 5px; 
     height: 60px;
     width: 60px;
     /* border: 1px solid rgb(255, 124, 1); */
 }
 
-.message-list .header .contact-name{
+.chat-content .message-list .header .contact-name{
     border-radius: inherit;
     padding-top: 26px;
     font-size: larger;
 }
 
-.message-list .header .play-button{
+.chat-content .message-list .header .play-button{
     padding: 11px;
     border-radius: inherit;
     display: flex;
     justify-content: end;
 }
 
-.message-list .header .play-button button{
+.chat-content .message-list .header .play-button button{
     border: none;
     background-color: #FFEEBF;
     border-radius: inherit;
@@ -330,32 +168,33 @@ i:hover{
 
 }
 
-.message-list .header .play-button button{
+.chat-content .message-list .header .play-button button{
     transition-duration: 0.5s;
 
 }
 
 
-.message-list .header .more-info{
+.chat-content .message-list .header .more-info{
     border-radius: inherit;
     padding-top: 21px;
 }
 
-.message-list .header .more-info .bi-three-dots-vertical{
+.chat-content .message-list .header .more-info .bi-three-dots-vertical{
     /* transform: rotate(90deg); */
     color: #2C3453;
 }
 
-.message-list .separator{
+.chat-content .message-list .separator{
     border-radius: 56px;
     background-color: #e2dede;
 }
 
 /*----------------------content-------------------------*/
  
-.message-list .content{
+.chat-content .message-list .content-k{
     background-color: transparent;
     grid-template-rows: 50% 50%;
+    border: 1px solid rgb(1, 43, 158);
     
     height: 27%; /*should be a variable depend of lenth of the message*/
     display: grid;
@@ -363,23 +202,23 @@ i:hover{
 
 /*----------------------sender-------------------------*/
 
-.message-list .content .sender{
+.chat-content .message-list .content-k .sender{
     padding: 15px;
     width: 50%;
     grid-template-columns: 12% 88%;
-    /* border: 1px solid rgb(1, 43, 158); */
-    /* gap : 1em; */
+    border: 1px solid rgb(1, 43, 158);
+    gap : 1em;
     display: grid;
 }
 
-.message-list .content .sender .sender-avatar{
+.chat-content .message-list .content-k .sender .sender-avatar{
     width: 55px;
     height: 55px;
     border-radius: 50px;
     background-color: #E8E9EC;
 }
 
-.message-list .content .sender .msg{
+.chat-content .message-list .content-k .sender .msg{
     border-radius: 70px 50px 50px 0px;
     background: rgba(255, 255, 255, 0.5);
     width: 90%;
@@ -389,14 +228,14 @@ i:hover{
     display: grid;
 }
 
-.message-list .content .sender .msg .msg-content{
+.chat-content .message-list .content-k .sender .msg .msg-content{
     color: #0A466E;
     padding: 16px;
     font-family: 'Josefin Sans Semibold';
     
 }
 
-.message-list .content .sender .msg .msg-time{
+.chat-content .message-list .content-k .sender .msg .msg-time{
     color: #9E9E9E;
     display: flex;
     justify-content: center;
@@ -406,7 +245,7 @@ i:hover{
 
 /*----------------------receiver-------------------------*/
 
-.message-list .content .receiver{
+.chat-content .message-list .content-k .receiver{
     padding: 15px;
     padding-left: 20px;
     height: 80px;
@@ -416,7 +255,7 @@ i:hover{
     /* border: 1px solid rgb(1, 43, 158); */
 }
 
-.message-list .content .receiver .msg{
+.chat-content .message-list .content-k .receiver .msg{
     border-radius: 50px 70px 0px 50px;
     background: rgba(255, 255, 255, 0.5);
     width: 90%;
@@ -427,14 +266,14 @@ i:hover{
 }
 
 
-.message-list .content .receiver .receiver-avatar{
+.chat-content .message-list .content-k .receiver .receiver-avatar{
     margin-left: 10px;
     width: 55px;
     height: 55px;
     border-radius: 50px;
     background-color: #E8E9EC;
 }
-.message-list .content .receiver .msg .msg-content{
+.chat-content .message-list .content-k .receiver .msg .msg-content{
     /* border: 1px solid rgb(255, 251, 2); */
     color: #0A466E;
     padding: 16px;
@@ -442,7 +281,7 @@ i:hover{
     
 }
 
-.message-list .content .receiver .msg .msg-time{
+.chat-content .message-list .content-k .receiver .msg .msg-time{
     /* border: 1px solid rgb(255, 251, 2); */
     color: #9E9E9E;
     display: flex;
@@ -453,7 +292,7 @@ i:hover{
 
 /*---------------------typing msg--------------------------*/
 
-.message-list .typing{
+.chat-content .message-list .typing{
     border-radius: 0px 0px 56px 56px ;
     background-color: #E8E9EC;
     grid-template-columns: 90% 10%;
@@ -461,7 +300,7 @@ i:hover{
 
 }
 
-.message-list .typing .input-msg{
+.chat-content .message-list .typing .input-msg{
     background-color: inherit;
     border-radius: 0px 0px 0px 56px;
     font-size: 16px;
@@ -471,7 +310,7 @@ i:hover{
     border: none;
 }  
 
-.message-list .typing .send-icon{    
+.chat-content .message-list .typing .send-icon{    
     /* border: 1px solid rgb(255, 251, 2); */
     /* border-radius: 0px 0px 56px 0px ; */
     display: flex;
@@ -525,20 +364,20 @@ i:hover{
     border-radius: 56px;
 }
 
-.chat-content .about-friend .content{
+.chat-content .about-friend .content-friend{
     grid-template-rows: 25% 8% 25% 10%;
     gap: 1em;
     display: grid;
     /* border: 1px solid rgb(34, 71, 69); */
 }
 
-.chat-content .about-friend .content .friend-info{
+.chat-content .about-friend .content-friend .friend-info{
     /* border: 1px solid rgb(34, 71, 69); */
     grid-template-rows: 80% 10%;
     display: grid;
 }
 
-.chat-content .about-friend .content .friend-info .avatar{
+.chat-content .about-friend .content-friend .friend-info .avatar{
     /* border: 1px solid rgba(255, 255, 255, 0.562); */
     width: 100px;
     height: 100px;
@@ -547,7 +386,7 @@ i:hover{
     background-color: #2C3453;
 }
 
-.chat-content .about-friend .content .friend-info .name{
+.chat-content .about-friend .content-friend .friend-info .name{
     display: flex;
     /* border: 1px solid rgb(34, 71, 69); */
     justify-content: center;
@@ -557,7 +396,7 @@ i:hover{
     align-items: center;
 }
 
-.chat-content .about-friend .content .level{
+.chat-content .about-friend .content-friend .level{
     /* border: 1px solid rgb(34, 71, 69); */
     width: 80%;
     margin:auto;
@@ -567,7 +406,7 @@ i:hover{
 
 }
 
-.chat-content .about-friend .content .options{
+.chat-content .about-friend .content-friend .options{
     /* border: 1px solid rgb(34, 71, 69); */
     grid-template-rows: 50% 25%;
     display: grid;
@@ -578,30 +417,30 @@ i:hover{
 
 }
 
-.chat-content .about-friend .content .options .fa-address-card{
+.chat-content .about-friend .content-friend .options .fa-address-card{
     color: #0A466E;
     justify-self: center;
     display: flex;
 }
 
-.view-profile{
+.chat-content .view-profile{
     margin-left: 25px;
 }
 
-.chat-content .about-friend .content .options .fa-user-lock{
+.chat-content .about-friend .content-friend .options .fa-user-lock{
     color: #0A466E;
     display: flex;
     color: #D44444;
 }
 
-.block-profile{
+.chat-content .block-profile{
     margin-left: 25%;
     color:  #0A466E;
     font-weight: 100;
 
 }
 
-.chat-content .about-friend .content .play{
+.chat-content .about-friend .content-friend .play{
     /* border: 1px solid rgb(34, 71, 69); */
     background-color: transparent;
     align-items: center;
@@ -609,7 +448,7 @@ i:hover{
     justify-content: center;
 }
 
-.chat-content .about-friend .content .play button{
+.chat-content .about-friend .content-friend .play button{
     background-color: #D44444;
     color: #D9DBBC;
     border-radius: 100px;
@@ -624,13 +463,18 @@ i:hover{
     filter: drop-shadow(0px 4px 4px rgba(212, 68, 68, 0.29));
 }
 
-.chat-content .about-friend .content .play button:hover{
+.chat-content .about-friend .content-friend .play button:hover{
     rotate: 360deg;
     transition: 0.6s;
     /* height: 60px; */
 }
 
 
-.hidden{
+.chat-content .hidden{
     display: none;
+}
+
+.chat-content .more-info img{
+    width: 24px;
+    height: 24px;
 }
