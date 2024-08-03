447a141d4e94a176daf5f316c68f2cb73d0b0597
Nisrin Boukhari <niboukha@c3r2p2.1337.ma>
2024-08-03 15:42:30 +0100

    fixed: merge single page

--- singlePage/script.js
+++ singlePage/script.js
@@ -1,153 +1,119 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
+function toggleChatSettings(){        
+    var icon = document.querySelector('.fa-ellipsis-vertical');
+    if (icon)
+    {
+        var hidden = document.querySelector('.hidden');
+        var chatContent = document.querySelector('.chat-content');
+        var msg_list = chatContent.querySelector('.message-list');
+        var msg_list_content = msg_list.querySelector('.content-msg-list');
+        var msg_list_header = msg_list.querySelector('.header');
+        var sender = msg_list_content.querySelector('.sender');
+        var typing_box = msg_list.querySelector('.typing'); 
+        var settings_friend = chatContent.querySelector('.about-friend');
+        
+        var toggleChatSettingsOn = function(){
+            
+            icon.style.display = "none";
+            hidden.style.display = "initial";
+            msg_list.style.borderRadius = '56px 0px 0px 56px'; 
+            msg_list_header.style.gap = '1em';
+            
+            settings_friend.style.borderRadius = '0px 56px 56px 0px'; 
+            chatContent.style.gap = '0em';
+            chatContent.style.gridTemplateColumns = '25% 1% 53% 21%';
+            settings_friend.style.display = "grid";
+    
+            typing_box.style.borderRadius = '0px 0px 0px 56px';
+    
+            var i = 0;
+            while (sender[i])
+            {
+                sender[i].style.gap = '1em';
+                i++;
+            }   
+        };
+    
+        var cancel_icon = document.querySelector('.fa-xmark');
+        
+        if (cancel_icon) 
+        {
+            var toggleChatSettingsOff = function(){
+                hidden.style.display = "none";
+                chatContent.style.gap = '1em';
+                chatContent.style.gridTemplateColumns = '25% 74%';
+                
+                msg_list.style.borderRadius = ''; 
+                msg_list_header.style.gap = '';
 
-// var hidden = document.querySelector('.hidden');
-// var icon = document.querySelector('.fa-ellipsis-vertical');
+                icon.classList.replace('fa-arrow-right', 'fa-ellipsis-vertical');   
+                icon.style.display = "initial";
+                
+                settings_friend.style.display = "none";
+                settings_friend.style.borderRadius = '';
+                typing_box.style.borderRadius = '';
+        
+                var i = 0;
+                while (sender[i])
+                {
+                    sender[i].style.gap = '';
+                    i++;
+                } 
+            };
+            cancel_icon.addEventListener('click', toggleChatSettingsOff);
+        }
+        icon.addEventListener('click', toggleChatSettingsOn);
+    }
+}
 
-// console.log(icon);
-// var chatContent = document.getElementsByClassName('chat-content');
-
-// if (chatContent[0]){
-//     var msg_list = chatContent[0].getElementsByClassName('message-list');
-//     var msg_list_content = msg_list[0].getElementsByClassName('content');
-//     var msg_list_header = msg_list[0].getElementsByClassName('header');
-//     var sender = msg_list_content[0].getElementsByClassName('sender');
-//     // var sender_msg = sender[0].getElementsByClassName('msg');
-//     var typing_box = msg_list[0].getElementsByClassName('typing'); 
-//     var settings_friend = chatContent[0].getElementsByClassName('about-friend');
-// }
-
-
-// var func = function(){
-//     if (chatContent && hidden && typing_box)
-//     {
-//         console.log("Custom logic executed");
-//         if (msg_list && settings_friend) {
-//             settings_friend[0].style.borderRadius = '0px 56px 56px 0px'; 
-//             msg_list[0].style.borderRadius = '56px 0px 0px 56px'; 
-//             msg_list_header[0].style.gap = '1em';
-
-//             hidden.style.display = "initial";
-//             chatContent[0].style.gap = '0em';
-//             chatContent[0].style.gridTemplateColumns = '25% 1% 53% 21%';
-//             settings_friend[0].style.display = "grid";
-//             console.log("Custom logic executed");
-//             icon.style.display = "none";
-
-//             typing_box[0].style.borderRadius = '0px 0px 0px 56px';
-
-//             var i = 0;
-//             while (sender[i])
-//             {
-//                 sender[i].style.gap = '1em';
-//                 i++;
-//             }   
-//         }
-//     }
-// };
-
-
-// if (icon)
-//     {
-//         icon.addEventListener('click', func);
-//         console.log("-----> ");
-//     }
-
-// var cancel_icon = document.querySelector('.fa-xmark');
-
-// if (cancel_icon) {
-//     var rollback = function(){
-
-//         chatContent[0].style.gap = '1em';
-//         chatContent[0].style.gridTemplateColumns = '25% 74%';
-//         settings_friend[0].style.display = "none";
-//         icon.classList.replace('fa-arrow-right', 'fa-ellipsis-vertical');   
-//         icon.style.display = "initial";
-//         hidden.style.display = "none";
-
-//         settings_friend[0].style.borderRadius = '';
-//         msg_list[0].style.borderRadius = ''; 
-//         typing_box[0].style.borderRadius = '';
-//         msg_list_header[0].style.gap = '';
-
-//         var i = 0;
-//             while (sender[i])
-//             {
-//                 sender[i].style.gap = '';
-//                 i++;
-//             } 
-
-//     };
-
-//     cancel_icon.addEventListener('click', rollback);
-//     console.log("Custom logic executed");
-// }
-// });
-
-
-
-// const icons = document.querySelectorAll('.icon');
-
-// icons.forEach(icon => {
-// icon.addEventListener('click', function() {
-//     this.classList.toggle('clicked');
-// });
-// });
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-// const hoverButton = document.getElementById('hoverButton');
-// if (hoverButton){
-//     hoverButton.addEventListener('mouseover', () => {
-//     hoverButton.style.backgroundColor = '#D44444';
-//     hoverButton.style.color = '#FFEEBF';
-//     });
-
-//     hoverButton.addEventListener('mouseout', () => {
-//     hoverButton.style.backgroundColor = '#FFEEBF';
-//     hoverButton.style.color = '#D44444';
-//     });
-// }
-
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-// const clickableDivs = document.querySelectorAll('.chat-item');
-
-// clickableDivs.forEach(div => {
-// div.addEventListener('click', () => {
-// clickableDivs.forEach(d => d.classList.remove('clicked-chat'));
-// div.classList.add('clicked-chat');
-// });
-// });
-// });
-
-
-
-// ------------------------single page----------------
-
+// ------------------------SINGLE PAGE---------------
 
 const urlRoute = {
-    "/":{template: "/pages/homePage/home.html"},
 
-    "chat":{template: "/pages/chat/chat.html"},
+    "/":{template: "/pages/landingPage/landingPage.html"},
+    // "/":{template: "/pages/homePage/home.html"},
+    "login":{template: "/pages/loginPage/loginPage.html"},
 
     "home":{template: "/pages/homePage/home.html"},
 
-    "acheivement":{template: "/pages/homePage/acheivement.html"},
+    "profile":{template: "/pages/profile/profile.html"},
 
-    "loginPage":{template: "/pages/homePage/loginPage.html"},
+    "chat":{template: "/pages/chat/chat.html"},
 
-    "playerVSplayer":{template: "/pages/homePage/playerVSplayer.html"},
+
+    "leaderboard":{template: "/pages/leaderboard/leaderboard.html"},
+
+
+    "playerVSplayer":{template: "/pages/playerVSplayer/playerVSplayer.html"},
+
+    "waitPlayerJoin":{template: "/pages/playerVSplayer/waitPlayerJoin.html"},
 
     404:{template: "/pages/homePage/404.html"},
 
-    "tournament":{ template: "/pages/homePage/tournament.html"}
+    "tournament":{ template: "/pages/tournament/tournament.html"},
+    "settings":{ template: "/pages/settings/settings.html"}
 }
 
 function loadPage(path)
 {
     const url = urlRoute[path].template;
-    const container = document.getElementById("global-content");
+    var div;
+    if(url=="/pages/landingPage/landingPage.html" || url=="/pages/loginPage/loginPage.html")
+    {
+
+        div = "landing-login";
+        document.querySelector("#all #container").style.display ="none";
+        document.querySelector("#all .content").style.display ="none";
+        document.querySelector("#all #landing-login").style.display ="initial";
+    }
+    else
+    {
+        document.querySelector("#all #container").style.display ="initial";
+        document.querySelector("#all .content").style.display ="grid";
+        document.querySelector("#all #landing-login").style.display ="none";
+        div = "global-content"
+    }
+    console.log("heeelo",url);
+    const container = document.getElementById(div);
     const request = new XMLHttpRequest();
 
     request.open("GET", url);
@@ -165,6 +131,7 @@ function loadPage(path)
             container.innerHTML = "<p>Page not found.</p>";
         }
         clickButton();
+        toggleChatSettings();
     }
 }
 
