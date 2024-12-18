04012b55d258854ce31657a726814b36ac5ff6f5
Souad Hicham <shicham@c4r5p1.1337.ma>
2024-12-18 11:04:59 +0100

    Fix:Resolved user API issues

---
+++ backend/Profile/consumers.py
@@ -0,0 +1,56 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from .models import *
+from django.shortcuts import render, get_object_or_404
+from asgiref.sync import sync_to_async
+# import asyncio
+# from concurrent.futures import ThreadPoolExecutor
+
+class RequestUpdateConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        print("------------------------------> here connected")
+        # print("---------__> hello the client is connected , "+ self.scope)
+        # self.req_status = self.scope['url_route']['kwargs']['status']
+        # self.req_id = self.scope['url_route']['kwargs']['id']
+        # print(">>>>>>>>>>>>>>>>>>> self.req_status " + self.req_status + "   self.req_id" + self.req_id)
+        await self.accept()
+
+    # async def disconnect(self, close_code):
+        # await self.channel_layer.group_discard(
+        #     self.room_group_name,
+        #     self.channel_name
+        # )
+    async def disconnect(self, close_code):
+        pass
+
+    # async def delete_request(request_id):
+    # # Use sync_to_async to perform the delete operation
+    #     await sync_to_async(Request.objects.filter)(id=request_id).delete()
+    
+    async def receive(self, text_data):
+        # await self.receive(text_data=message["text"])
+        # self.req_id = self.scope['url_route']['kwargs']['status']
+        # self.req_id = self.scope['url_route']['kwargs']['id']
+        text_data_json = json.loads(text_data)
+        print("---------> data recieved : ", text_data_json)
+        message = text_data_json['message']
+        # targetRequest =  
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> id : ", text_data_json['id'])
+        # id = 
+        request = await sync_to_async(lambda: Request.objects.get(id=text_data_json['id']))()      
+        # request = await sync_to_async(Request.objects.get)(id=text_data_json['id'])
+        # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> request : ", request)
+        # await sync_to_async(request.delete)()        
+        # sender = await get_sender(request)
+        # reciever = await get_reciever(request)
+        # print(">>>>>>>>>>>>>>>>>>>>> target req : ", result)
+        # result.delete()
+        # print("-----------------------> recieved data ", message)
+        # Send message to room group
+        # await self.channel_layer.group_send(
+        #     self.room_group_name,
+        #     {
+        #         'type': 'chat_message',
+        #         'message': message
+        #     }
+        # )
---
+++ backend/project/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+from Profile.urls import websocket_urlpatterns  # Adjust according to your app name
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # Handle traditional HTTP requests
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns # Specify your WebSocket URL patterns
+        )
+    ),
+})
